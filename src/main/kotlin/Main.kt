16dcaddc95d21353f4010de687198e28600aaa83
= <redalsdn7@naver.com>
2022-04-14 08:06:05 +0900

    main 함수의 구현을 클래스의 함수로 분리

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+import scrren.ShoppingHome
+
+fun main(args: Array<String>) {
+
+    /*
+    Step 1. main 함수 -> 클래스의 함수 (O)
+    Step 2. 화면별 클래스 분리
+    Step 3. 선택한 카테고리 상품 목록 표시
+     */
+
+    val shoppingHome = ShoppingHome()
+    shoppingHome.start()
+}
