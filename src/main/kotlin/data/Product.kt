8700999422f46c745fa341246ef7df6380c2c2cb
= <redalsdn7@naver.com>
2022-04-17 21:23:08 +0900

    product 객체 추가 및 ProductList 화면이 상품출력 담당

---
+++ src/main/kotlin/data/Product.kt
@@ -0,0 +1,7 @@
+package data
+
+// 상품 정보 data class
+data class Product(
+    var categoryLavel: String,
+    val name:String
+)
--- src/main/kotlin/scrren/ShoppingCategory.kt
+++ src/main/kotlin/scrren/ShoppingCategory.kt
@@ -22,6 +22,8 @@ class ShoppingCategory {
         } else {
             if (categories.contains(selectedCategory)) {
                 // TODO 카테고리 상품 목록 보여주기
+                val shoppingProductList = ShoppingProductList()
+                shoppingProductList.showProducts(selectedCategory)
             } else {
                 showErrorMessage(selectedCategory)
             }
